90d30e9d97b533e1ee3c22e5b65480037a022680
Agaphon1888 <egorov2000@inbox.ru>
2024-12-15 14:20:36 +0500

    Домашнее задание по теме "Систематизация и пропуск тестов". Задача "Заморозка кейсов".

---
+++ suite_12_3.py
@@ -0,0 +1,19 @@
+# Домашнее задание по теме "Систематизация и пропуск тестов".
+# Задача "Заморозка кейсов".
+
+import unittest
+from tests_12_3 import TournamentTest  # Импортируйте ваш тестовый класс
+
+# Создаем объект TestSuite
+class TestSuite:
+    def __init__(self):
+        self.suite = unittest.TestSuite()
+        self.suite.addTest(unittest.makeSuite(TournamentTest))
+
+    def run(self):
+        runner = unittest.TextTestRunner(verbosity=2)
+        runner.run(self.suite)
+
+if __name__ == '__main__':
+    test_suite = TestSuite()
+    test_suite.run()
